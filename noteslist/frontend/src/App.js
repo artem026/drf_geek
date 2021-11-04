e16fe6f99033fc6e4d2c2f120380341446118113
Artem026 <kris_my@mail.ru>
2021-11-04 19:18:27 +0300

    hw2_add_app

--- noteslist/frontend/src/App.js
+++ noteslist/frontend/src/App.js
@@ -1,25 +1,44 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import AuthorList from "./components/Author.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'authors': []
+    }
+  }
+
+  componentDidMount() {
+    const authors = [
+      {
+        'first_name': 'Федор',
+        'last_name': 'Достоевский',
+        'birthday_year': 1821
+      },
+      {
+        'first_name': 'Михаил',
+        'last_name': 'Лермонтов',
+        'birthday_year': 1814
+      }
+    ]
+    this.setState(
+      {
+        'authors':authors
+      }
+    )
+  }
+
+
+  render() {
+    return (
+      <div>
+        <AuthorList authors={this.state.authors}/>
+      </div>
+    )
+  }
 }
 
 export default App;
